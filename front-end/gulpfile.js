ee8aca6be3ada3cb6f1b103ba8abd325c9dd4511
maczyt <369280416@qq.com>
2016-06-23 22:39:28 +0800

    yun 2.0

--- front-end/gulpfile.js
+++ front-end/gulpfile.js
@@ -1,38 +1,26 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	uglify = require('gulp-uglify'),
-	minifyCSS = require('gulp-minify-css'),
-	browserSync = require('browser-sync').create(),
-	reload = browserSync.reload;
+    sass = require('gulp-ruby-sass'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    minifyCSS = require('gulp-minify-css'),
+    uglify = require('gulp-uglify');
 
-gulp.task('sass-watch', function () {
-	return gulp.src('sass/*.scss')
-		.pipe(sass()) 
-		.pipe(minifyCSS({keepBreaks: false}))
-		.pipe(gulp.dest('dist/css'))
-		.pipe(reload({stream: true}))
-		.on('error', sass.logError);
-});
-gulp.task('js', function () {
-	return gulp.src('js/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'))
-		.on('error', function () {});
-})
-gulp.task('js-watch', ['js'], reload);
-
-
-// 开启一个服务器
-gulp.task('serve', function () {
-	// 从这个项目的根目录启动服务器
-	browserSync.init({
-		server: {
-			baseDir: './'
-		}
-	});
-	gulp.watch('sass/*.scss', ['sass-watch']);
-	gulp.watch('js/*.js', ['js-watch']);
-	gulp.watch('./**/*.html').on('change', reload);
+gulp.task('sass', function () {
+    return sass('sass/*.scss')
+        .pipe(minifyCSS({keepBreaks: false}))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(reload({stream: true}))
+        .on('error', sass.logError);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('browserSync', ['sass'], function () {
+    browserSync.init({
+        server: "./",
+        files: ['dist/*.css', '*.html', 'js/*.js']
+    });
+    gulp.watch('sass/*.scss', ['sass']);
+    gulp.watch('*.html').on('change', reload);
+    gulp.watch('js/*.js').on('change', reload);
+});
+
+gulp.task('default', ['browserSync']);
